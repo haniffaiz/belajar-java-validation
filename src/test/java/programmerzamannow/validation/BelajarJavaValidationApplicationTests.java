fa1b32ff3a49b465ee6f109f1cb2032cba2c8f1c
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-17 04:31:38 +0700

    Initial commit

---
+++ src/test/java/programmerzamannow/validation/BelajarJavaValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package programmerzamannow.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
