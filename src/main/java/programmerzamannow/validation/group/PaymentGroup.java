5f04a032d29d87461dab366acdb938478b178564
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-18 01:10:13 +0700

    group sequence

---
+++ src/main/java/programmerzamannow/validation/group/PaymentGroup.java
@@ -0,0 +1,16 @@
+package programmerzamannow.validation.group;
+
+import jakarta.validation.GroupSequence;
+import jakarta.validation.groups.Default;
+
+@GroupSequence(value = {
+        Default.class,
+        CreditCardPaymentGroup.class,
+        VirtualAccountPaymentGroup.class
+})
+
+public interface PaymentGroup {
+
+
+
+}
