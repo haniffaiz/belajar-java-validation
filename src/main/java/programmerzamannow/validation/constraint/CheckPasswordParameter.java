629cfdbf4bc6261958efce91ef6245fda08dc0a4
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-18 19:25:49 +0700

    cross parameter constraint

---
+++ src/main/java/programmerzamannow/validation/constraint/CheckPasswordParameter.java
@@ -0,0 +1,28 @@
+package programmerzamannow.validation.constraint;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+
+@Documented
+@Constraint(
+        validatedBy = {CheckPasswordParameterValidator.class}
+)
+@Target({ElementType.FIELD, ElementType.ANNOTATION_TYPE,ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+
+
+public @interface CheckPasswordParameter {
+
+    int passwordParam();
+
+    int retypePasswordParam();
+
+    String message() default "password and retype password must same";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
